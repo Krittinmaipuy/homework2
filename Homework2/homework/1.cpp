4197a6803384839c3bd0410e499216d22cd60087
Krittinmaipuy <77138513+Krittinmaipuy@users.noreply.github.com>
2021-01-14 11:20:50 +0700

    Add files via upload

---
+++ Homework2/homework/1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	cout<<"1st Term of 2020         /6206021611168    Krittin maipuy                     "<<endl;
+
+	cout<<"------------------------------------------------------------------------------"<<endl;
+	cout<<"|  subject  ID  | Subject name              | Score  (100) |    Grad    |"<<endl;
+	cout<<"------------------------------------------------------------------------------"<<endl;
+	cout<<"|  4570034      | Introduction  to computer |      85      |    A       |"<<endl;
+	cout<<"|  4580247      | English                   |      74      |    B       |"<<endl;
+	cout<<"|  4530001      | Design Thinking           |      92      |    A       |"<<endl;
+	cout<<"------------------------------------------------------------------------------"<<endl;
+	cout<<"|  TOTAL        |Subject = 3                |   Average =3.87 |"<<endl;
+	cout<<"====================================*==="<<endl;
+
+
+
+
+
+	cout<<"KMUTNB"<<endl;
+
+}
